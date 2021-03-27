bcb557e1c02cd5e94979978845db35d0a5130def
sadDarkHorse <47600105+sadDarkHorse@users.noreply.github.com>
2021-03-28 00:21:07 +0800

    Create 3(标答).py

---
+++ 课后作业/实验(第2章)/3(标答).py
@@ -0,0 +1,18 @@
+#使用turtle库，绘制一个边长为100的八角图形。
+#方法一
+import turtle
+turtle.fd(100)
+turtle.seth(135)
+turtle.fd(100)
+turtle.seth(-90)
+turtle.fd(100)
+turtle.seth(45)
+turtle.fd(100)
+turtle.seth(180)
+turtle.fd(100)
+turtle.seth(-45)
+turtle.fd(100)
+turtle.seth(90)
+turtle.fd(100)
+turtle.seth(-135)
+turtle.fd(100)
