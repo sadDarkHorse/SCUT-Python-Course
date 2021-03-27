755ed48b2542770c06524a1be374ee7860ed1077
sadDarkHorse <47600105+sadDarkHorse@users.noreply.github.com>
2021-03-28 00:21:45 +0800

    Create 3(自制).py

---
+++ 课后作业/实验(第2章)/3(自制).py
@@ -0,0 +1,18 @@
+#使用turtle库，绘制一个边长为100的八角图形。
+import turtle
+turtle.seth(0)
+turtle.fd(100)
+turtle.seth(135)
+turtle.fd(100)
+turtle.seth(270)
+turtle.fd(100)
+turtle.seth(45)
+turtle.fd(100)
+turtle.seth(180)
+turtle.fd(100)
+turtle.seth(315)
+turtle.fd(100)
+turtle.seth(90)
+turtle.fd(100)
+turtle.seth(225)
+turtle.fd(100)
