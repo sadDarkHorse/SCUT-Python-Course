666da5dd217db47a12d5d7dcffe3d7940e867568
sadDarkHorse <sadlovewafer@gmail.com>
2021-05-18 21:16:57 +0800

    Update 1（自制）.py

--- 课后作业/实验(第7章_2)/1（自制）.py
+++ 课后作业/实验(第7章_2)/1（自制）.py
@@ -1,6 +1,6 @@
 '''【1】 (程序设计)编写程序，计算1到100的阶乘，把阶乘结果保存到文本文件“阶乘.txt”。'''
 fo=open('阶乘.txt','w')
-for i in range(101):
+for i in range(1,101):
     result=1
     for j in range(i):
         result*=(j+1)
