daf49cdd539d8063b27e8b34f3d189ac9d4257b5
sadDarkHorse <sadlovewafer@gmail.com>
2021-06-28 13:45:22 +0800

    Create 28.py

---
+++ 课堂小测/模拟试卷2/28.py
@@ -0,0 +1,14 @@
+#28、(程序设计)编写程序，输入正整数a和b，产生a个范围在[100,999]的随机整数。输出这a个随机数(每行5个，数据之间用一个空格隔开)，找出能被b整除的数据个数。注意：不能使用列表、元组、集合、字典等组合数据类型。
+import random
+a=eval(input('请输入整数a：'))
+b=eval(input('请输入整数b：'))
+d=0
+for i in range(1,a+1):
+    c=random.randint(100,999)
+    if i%5==0:
+        print(c)
+    else:
+        print('{} '.format(c),end='')
+    if c%b==0:
+        d+=1
+print('总共{}个数能被{}整除'.format(d,b))
